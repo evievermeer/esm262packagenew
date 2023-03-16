e96fde28298a426db452b9e984aabfde7321ee2f
evievermeer <evermeer@bren.ucsb.edu>
2023-03-16 14:10:11 -0700

    Updated data documentation

--- R/fish_data_documentation.R
+++ R/fish_data_documentation.R
@@ -1,3 +1,10 @@
-#' Documentation for fish data
+#' Fish stock and harvest data for a hypothetical fisheries model
+#'
+#' Random data produced using the 'rnorm' function with 100 observations.
+#' Stock data has a mean value of 100,000 (kg)
+#' Harvest data (annual) has a mean value of 10,000 (kg)
+#'
+#'
+#' @format A data frame with 101 rows and 2 variables.
 #'
 "fish_data"
